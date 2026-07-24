4fdc141d711f0042294408b91201f980d3896930
naisor <robertocaballerode@gmail.com>
2026-07-24 15:12:08 -0500

    Version 2003 prueba
    Ya casi se logra la app base

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "02.10.0608.2003"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
